3a71ed9989c58bd9cfdcf5ef25e835b08d90db12
Sherriff Agboola <rylxes@gmail.com>
2021-07-15 18:59:15 +0100

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::environment('heroku')) {
-            Log::useFiles('php://stderr');
-        }
+//        if (App::environment('heroku')) {
+//            Log::useFiles('php://stderr');
+//        }
     }
 }
